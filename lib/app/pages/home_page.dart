87d83805f2e6c7ab14925c80d166a11cc770a48e
Yago Taveiros Ferreira <ytf@ic.ufal.br>
2023-05-13 23:26:03 -0300

    Update home_page.dart

--- lib/app/pages/home_page.dart
+++ lib/app/pages/home_page.dart
@@ -23,14 +23,14 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             children: [
               const SizedBox(height: 32),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
+                      children: [
                         Text(
                           'Olá Claudio',
                           style: PhoneplanetTextStyles.title,
@@ -42,7 +42,7 @@ class _HomePageState extends State<HomePage> {
                         ),
                       ],
                     ),
-                    const PhoneplanetIconButton(
+                    PhoneplanetIconButton(
                       icon: Icon(
                         Icons.shopping_bag_outlined,
                       ),
@@ -138,11 +138,11 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
               const SizedBox(height: 18),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
+                  children: [
                     Expanded(
                       child: PhoneplanetProductCard(
                         image:
@@ -184,11 +184,11 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
               const SizedBox(height: 18),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
+                  children: [
                     Expanded(
                       child: PhoneplanetProductCard(
                         image:
