3713b03be3125014fc8ee1a73fb83c389340cbcc
Yago Taveiros Ferreira <ytf@ic.ufal.br>
2023-05-14 01:47:28 -0300

    Update api.dart

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,2 +1,12 @@
+// Services
+export 'package:phoneplanet/api/service/service.dart';
 export 'package:phoneplanet/api/service/main_service.dart';
+
+// Repositories
 export 'package:phoneplanet/api/repositories/product_repository.dart';
+
+// Models
+export 'package:phoneplanet/api/models/product.dart';
+
+// Exceptions
+export 'package:phoneplanet/api/exceptions/api_exception.dart';
